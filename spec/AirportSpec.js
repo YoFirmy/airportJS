f9e70d722d72e567fcc8ef772a8c8f92a95a258e
Matt Firmston <mattfirmston@gmail.com>
2021-08-03 15:17:02 +0100

    Plane can land at airport

---
+++ spec/AirportSpec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+describe("Aiport", () => {
+  let airport;
+  let plane;
+  
+  beforeEach(() => {
+    airport = new Airport();
+    plane = jasmine.createSpy("plane", ["land"]);
+  });
+
+  it("has not planes by default", () => {
+    expect(airport.planes()).toEqual([]);
+  });
+
+  it("can clear planes for landing", () => {
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
+  });
+});
