8c8e3a70e37a0149e9d7afff4ded1fb935c58192
Matt Firmston <mattfirmston@gmail.com>
2021-08-03 19:10:56 +0100

    planes are prevented from taking off during a storm

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -23,4 +23,25 @@ describe("Aiport", () => {
     airport.clearForTakeOff(plane);
     expect(airport.planes()).toEqual([]);
   });
+
+  it("can check for stormy conditions", () => {
+    expect(airport.isStormy()).toBeFalsy();
+  });
+
+
+});
+
+describe("under stormy conditions", () => {
+  let airport;
+  let plane;
+  
+  beforeEach(() => {
+    airport = new Airport();
+    plane = jasmine.createSpy("plane", ["land"]);
+  });
+  
+  it("does not clear planes for takeoff", () => {
+    spyOn(airport,'isStormy').and.returnValue(true)
+    expect(() => { airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+  });
 });
